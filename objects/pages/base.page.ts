34afb5e3ed8f64ad9cdab7263c1b68ff27790c08
Nenad Trajanovski <nenad.trajanovski@mac141.local>
2026-01-05 11:19:04 +0100

    second session

---
+++ objects/pages/base.page.ts
@@ -0,0 +1,9 @@
+import { APIRequestContext, Page } from "@playwright/test";
+
+class BasePage {
+    page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+} export default BasePage;
--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,12 +1,34 @@
-import { test } from '@playwright/test';
+import LoginPage from '@objects/pages/login.page';
+import { expect, test } from '@playwright/test';
 
 test.describe('User testing Suite', () => {
+
+    test.beforeEach(async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await page.goto('http://localhost:8080/login');
+        await loginPage.loginUser(`automation@test.com`, `temp123`);
+        await page.waitForSelector('//h1', {timeout: 5000});
+    })
+    
     
     test('User logged in successfully', async ({ page }) => {
-        await page.goto('http://localhost:8080/login');
-        await page.fill(`input#email`, 'automation@test.com');
-        await page.fill('input#password', 'temp123');
-        await page.locator(`//button[text() = 'Sign In']`).click();
-        await page.waitForSelector('//h1', {timeout: 5000});
+        expect(await page.locator('//h1').textContent()).toBe('Dashboard');
+    });
+
+    test('Validate mandatory fields', async ({ page }) => {
+        await page.locator(`//button/span[text() = 'Add a Book']`).click();
+        await page.locator(`//button[text() = 'Create']`).click();
+        await page.waitForSelector(`div#root li div div:nth-child(2)`, {timeout: 5000});
+
+        expect(await page.locator(`div#root li div div:nth-child(2)`).first().innerText()).toBe(`Please fill in both title and author fields`);
+    });
+
+    test('Add a book', async ({ page }) => {
+        await page.locator(`//button/span[text() = 'Add a Book']`).click();
+        await page.locator(`//input[@placeholder = 'Enter author name']`).fill(`Automation`);
+        await page.locator(`//input[@placeholder = 'Enter book title']`).fill(`QA`);
+        await page.locator(`//button[text() = 'Create']`).click();
+
+        expect(await page.locator(`//h3[text() = 'QA']`)).toBeVisible();
     });
 });
