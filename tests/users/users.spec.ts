c34be0bc1ae8b7a370b60a2c80f3e32fa0a23f5d
Tea Nikolov <tea.nikolov@scalefocus.com>
2026-01-11 18:37:14 +0100

    add three more tests

--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,3 +1,8 @@
+import AddBookComponent from '@objects/components/addBook.component';
+import AddPersonComponent from '@objects/components/addPerson.component';
+import BookRowComponent from '@objects/components/bookRow.component';
+import LibraryComponent from '@objects/components/library.component';
+import ManagePeopleComponent from '@objects/components/managePeople.component';
 import LoginPage from '@objects/pages/login.page';
 import { expect, test } from '@playwright/test';
 
@@ -5,6 +10,7 @@ test.describe('User testing Suite', () => {
 
     test.beforeEach(async ({ page }) => {
         const loginPage = new LoginPage(page);
+
         await page.goto('http://localhost:8080/login');
         await loginPage.loginUser(`automation@test.com`, `temp123`);
         await page.waitForSelector('//h1', {timeout: 5000});
@@ -24,11 +30,69 @@ test.describe('User testing Suite', () => {
     });
 
     test('Add a book', async ({ page }) => {
-        await page.locator(`//button/span[text() = 'Add a Book']`).click();
-        await page.locator(`//input[@placeholder = 'Enter author name']`).fill(`Automation`);
-        await page.locator(`//input[@placeholder = 'Enter book title']`).fill(`QA`);
-        await page.locator(`//button[text() = 'Create']`).click();
-
-        expect(await page.locator(`//h3[text() = 'QA']`)).toBeVisible();
+        const bookTitle = "QA";
+        const authorName = "Automation";
+        const libraryComponent = new LibraryComponent (page);
+        const addBookComponent = new AddBookComponent (page);
+        const bookRowComponent = new BookRowComponent (page);
+        await libraryComponent.clickAddBook();
+        expect(addBookComponent.dialogHeader).toBeVisible();
+        await addBookComponent.enterAuthorName(authorName);
+        await addBookComponent.enterBookTitle(bookTitle);
+        await addBookComponent.createBookRecord();
+        expect(await bookRowComponent.getTaskTitle(bookTitle)).toEqual(authorName + " - " + bookTitle);
     });
+    
+    test('Add a new person', async ({ page }) => {
+        const newPerson = "Tst-2406-1";
+        const addNewPersonComponent = new AddPersonComponent (page);
+        const libraryComponent = new LibraryComponent (page);
+        const managePeopleComponent = new ManagePeopleComponent (page);
+        await libraryComponent.clickAddPerson();
+        await addNewPersonComponent.enterPersonName(newPerson);
+        await addNewPersonComponent.addPerson();
+
+        //Assert
+        await libraryComponent.clickManagePeople();
+        expect(await managePeopleComponent.isPersonPresent(newPerson)).toBe(true);
+
+        //Delete the new created person - this should be with API call
+        await managePeopleComponent.deletePerson(newPerson);
+    });
+
+    test('Delete a person successfully', async ({page})=>{
+        const newPerson = "Tst-2406-2";
+        const addNewPersonComponent = new AddPersonComponent (page);
+        const libraryComponent = new LibraryComponent (page);
+        const managePeopleComponent = new ManagePeopleComponent (page);
+        await libraryComponent.clickAddPerson();
+        await addNewPersonComponent.enterPersonName(newPerson);
+        await addNewPersonComponent.addPerson();
+
+        await libraryComponent.clickManagePeople();
+        await managePeopleComponent.deletePerson(newPerson);
+        expect (await managePeopleComponent.isPersonPresent(newPerson)).toBe(false)
+
+    })
+
+    test('Mark the book as completed', async ({page}) => {
+        const bookTitle = "Luna";
+        const authorName = "Vinka Sazdova";
+        const libraryComponent = new LibraryComponent (page);
+        const addBookComponent = new AddBookComponent (page);
+        const bookRowComponent = new BookRowComponent (page);
+        await libraryComponent.clickAddBook();
+        expect(addBookComponent.dialogHeader).toBeVisible();
+        await addBookComponent.enterAuthorName(authorName);
+        await addBookComponent.enterBookTitle(bookTitle);
+        await addBookComponent.createBookRecord();
+        expect(await bookRowComponent.getTaskTitle(bookTitle)).toEqual(authorName + " - " + bookTitle);
+        await bookRowComponent.toggleTaskCheckbox(bookTitle);
+
+        //assert
+        expect(await bookRowComponent.isTaskCompleted(authorName, bookTitle)).toBe(true);
+        // await bookRowComponent.deleteTask(bookTitle);
+    })
+
+
 });
